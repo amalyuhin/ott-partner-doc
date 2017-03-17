647a1b471ab338c717b5b8b26a637a651e0267f9
kotov.den@gmail.com <26641988Mart>
2017-03-17 14:01:29 +0700

    OTT-588 added generator for all docs file

--- compile.js
+++ compile.js
@@ -6,22 +6,72 @@ var yamljs = require('yamljs');
 var mkdirp = require('mkdirp');
 
 
-glob( 'json/**/*.json', function( err, files ) {
-    files.forEach(function (file) {
-        var data = require(path.resolve(__dirname, file));
 
-        var yamlString = yamljs.stringify(data, 100000, 3);
-        var newFileName = file.replace('json/', 'yml/').replace('.json', '.yml');
+if(makeAllDocs()) {
+    json2yml();
+}
 
-        mkdirp(path.dirname(newFileName), function (err) {
-            if (err) return console.error(err);
+function json2yml() {
+    glob( 'json/**/*.json', function( err, files ) {
+        files.forEach(function (file) {
+            var data = require(path.resolve(__dirname, file));
 
-            fs.writeFile(newFileName, yamlString, function (err) {
-                if (err) return console.log(err);
-                console.log('successful for ' + newFileName);
+            var yamlString = yamljs.stringify(data, 100000, 3);
+            var newFileName = file.replace('json/', 'yml/').replace('.json', '.yml');
+
+            mkdirp(path.dirname(newFileName), function (err) {
+                if (err) return console.error(err);
+
+                fs.writeFile(newFileName, yamlString, function (err) {
+                    if (err) return console.log(err);
+                    console.log('successful for ' + newFileName);
+                });
+            });
+        });
+    });
+}
+
+function makeAllDocs() {
+    var allDocs = {};
+    var success = true;
+    glob( 'json/**/*.json', function( err, files ) {
+        var itemCount = 0;
+        files.forEach(function (file) {
+            var data = require(path.resolve(__dirname, file));
+            console.log(file);
+            if (itemCount == 0) {
+                allDocs = data;
+            }
+            else {
+                setPathsInAllDocs(data);
+            }
+            itemCount++;
+        });
+        mkdirp('./json/avia/allDocs', function (err) {
+            if (err) {
+                success = false;
+                return console.error(err);
+            }
+
+            fs.writeFile('./json/avia/allDocs/docs.json', JSON.stringify(allDocs), function (err) {
+                if (err) {
+                    success = false;
+                    return console.error(err);
+                }
+                console.log('successful for all docs');
             });
         });
 
     });
-});
+
+    function setPathsInAllDocs(data) {
+        var paths  =  data.paths;
+        for (var path in paths) {
+            if (paths.hasOwnProperty(path)) {
+                allDocs.paths[path] = paths[path];
+            }
+        }
+    }
+    return success;
+}
 
